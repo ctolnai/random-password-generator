2d6513fab09ecc392f37ae1cb1dfe1f7625cf39a
Chris Tolnai <christolnai@gmail.com>
2021-08-12 17:10:05 -0700

    basis for function to ask user preference for password

--- script.js
+++ script.js
@@ -3,6 +3,40 @@ var numericalCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
 var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" , "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+function askUserForPreference() {
+  var userPreference = []
+    if(userPreference === "")
+
+    if(userPreference > 7)
+
+    if(userPreference < 129)
+  
+  var userSpecialCharacterConfirmation = confirm("Do you want to have a special character in your password");
+    if (userSpecialCharacterConfirmation == true){
+      userSpecialCharacter.log = "true";
+    }
+    else { userSpecialCharacter.log = "false";
+
+    }
+
+
+
+    var userSpecialCharacter = []
+    var userNumericalCharacter = []
+    var userUppercaseCharacter = []
+    var userLowecaseCharacter = []
+  return passwordOptions;
+
+
+
+}
+
+
+
+
+
+
+
 
 
 // Assignment Code
@@ -19,6 +53,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-console.log(specialCharacters[4])
