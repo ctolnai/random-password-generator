542ef4bd25993d3235da08310d15aa80bb4f3288
Chris Tolnai <christolnai@gmail.com>
2021-08-12 18:03:35 -0700

    fixed generate password prompts

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
-var numericalCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
-var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" , "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numericalCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-function askUserForPreference() {
-  var userPreference = []
-    if(userPreference === "")
+function generatePassword() {
+  var passwordLength = window.prompt("Chose a number between 8 and 128");
+  while (passwordLength <= 7 || passwordLength >= 129) {
+    passwordLength = window.prompt("Chose a number between 8 and 128");
 
-    if(userPreference > 7)
+  }
+  console.log(passwordLength);
 
-    if(userPreference < 129)
-  
   var userSpecialCharacterConfirmation = confirm("Do you want to have a special character in your password");
-    if (userSpecialCharacterConfirmation == true){
-      userSpecialCharacter.log = "true";
-    }
-    else { userSpecialCharacter.log = "false";
-
-    }
+  if (userSpecialCharacterConfirmation == true) {
+  }
+  console.log(userSpecialCharacterConfirmation);
 
 
+  var userNumericalCharacterConfirmation = confirm("Do you want to have a numerical character in your password");
+  if (userNumericalCharacterConfirmation == true) {
+  }
+  console.log(userNumericalCharacterConfirmation);
 
-    var userSpecialCharacter = []
-    var userNumericalCharacter = []
-    var userUppercaseCharacter = []
-    var userLowecaseCharacter = []
-  return passwordOptions;
+
+  var userUppercaseCharacterConfirmation = confirm("Do you want to have a numerical character in your password");
+  if (userUppercaseCharacterConfirmation == true) {
+  }
+  console.log(userUppercaseCharacterConfirmation);
+
+
+  var userLowercaseCharacterConfirmation = confirm("Do you want to have a numerical character in your password");
+  if (userLowercaseCharacterConfirmation == true) {
+  }
+  console.log(userLowercaseCharacterConfirmation);
+
+  var userSelection = {
+    passwordlength,
+    userSpecialCharacterConfirmation,
+    userNumericalCharacterConfirmation,
+    userUppercaseCharacterConfirmation,
+    userLowercaseCharacterConfirmation,
+
+  }
 
 
 
 }
+
+
 
 
 
